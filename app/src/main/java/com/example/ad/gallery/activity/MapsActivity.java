9c89f74605bed1d119233816b4daccad8ace2a22
HASWELL\truongbonba <truongbonba95@gmail.com>
2017-04-01 15:10:19 +0700

    latest

--- app/src/main/java/com/example/ad/gallery/activity/MapsActivity.java
+++ app/src/main/java/com/example/ad/gallery/activity/MapsActivity.java
@@ -1,8 +1,9 @@
-package com.example.ad.gallery;
+package com.example.ad.gallery.activity;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.example.ad.gallery.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
